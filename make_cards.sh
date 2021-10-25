c9272afc5d3b851286cf36028e01fc5b431faed2
Javier Duarte <jduarte@ucsd.edu>
2021-10-24 20:12:12 -0700

    fix

--- make_cards.sh
+++ make_cards.sh
@@ -6,5 +6,5 @@ fi
 python create_datacard_test.py --carddir $DIRECTORY --inputfile $3/HHTo4BPlots_Run2_BDT$1.root
 cd $DIRECTORY
 
-. ../../build_final_test.sh  $1 $2
-cd ../../
+. ../build_final_test.sh  $1 $2
+cd ../
